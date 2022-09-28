f4aa1cb69325567e75d1f1ff044fa44166f3bbf6
wniebelski <wniebelski@gmail.com>
2022-09-28 09:10:58 +0200

    rozwiązanie

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const p1 = 10
+const p2 = 20
+const p3 = 30
+const p4 = 40
+const p5 = 50
+var arr = [p1,p2,p3,p4,p5];
+function tabletum (tab) {
+    return tab[0];
+}
+console.log (tabletum (arr));
